b973f83f739f5f42fc55f5175c9b22714dfc964d
Corbin <forstupidityonly@gmail.com>
2024-01-23 10:23:28 -0600

    Im using git correctly for personal projects smh

--- testing.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-import pandas as pd
-import numpy as np
-from flatten_json import flatten
-
-r = requests.get('https://npiregistry.cms.hhs.gov/api/?number=&enumeration_type=&taxonomy_description=&first_name=&last_name=&organization_name=&address_purpose=&city=&state=&postal_code=90201&country_code=&limit=10&skip=&version=2.0')
-results_text = r.text
-print(results_text)
